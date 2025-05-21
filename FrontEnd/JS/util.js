6653fa29dde6dd0f1ba0ae627dcb90f70534cb26
Neal Muller <neal.henri.muller@gmail.com>
2025-05-21 18:50:38 +0200

    Ajout des fonctions utilisées pour le formulaire

--- FrontEnd/JS/util.js
+++ FrontEnd/JS/util.js
@@ -27,3 +27,57 @@ export function filterBtn(works) {
     });
   }
 }
+
+/**
+ * La fonction envoie les information de logIn en format JSON à l'API (POST)
+ * @param {string} emailLog : l'email de l'utilisateur
+ * @param {string} passwordLog : le mot de passe de l'utilisateur
+ * @returns {object} un objet javascript contenant le token d'authentification
+ */
+export async function logIn(emailLog, passwordLog) {
+  const dataLogIn = {
+    email: emailLog,
+    password: passwordLog,
+  };
+  const chargeUtile = JSON.stringify(dataLogIn);
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: chargeUtile,
+  });
+
+  if (!response.ok) {
+    console.log("Erreur de connexion");
+  } else {
+    const data = response.json();
+    return data;
+  }
+}
+
+/**Cette fonction enlève les espacements dans une chaine de caractère
+ * @param {string} texte : le texte saisi dans les inputs du formulaire de log in
+ * @returns {string} : le texte saisi sans les espaces
+ */
+export function nettoyageEspacement(texte) {
+  let texteSansEspace = texte.replace(/\s+/g, "");
+  return texteSansEspace;
+}
+// Cette fonction affiche le popup en changeant la class
+// Passe de "inactive" à "active"
+export function afficherPopUp() {
+  const divPopup = document.querySelector(".popup");
+  const textErrorConteneur = document.querySelector(".popup__txt");
+  const textError = document.createElement("p");
+
+  textError.innerText = "Le mot de passe ou l'email est invalide";
+  textErrorConteneur.appendChild(textError);
+  divPopup.classList.remove("inactive");
+  divPopup.classList.add("active");
+}
+//Cette fonction masque le popUp en changeant la class
+// Passe de "active" à "inactive"
+export function closePopUp() {
+  const divPopup = document.querySelector(".popup");
+  divPopup.classList.remove("active");
+  divPopup.classList.add("inactive");
+}
