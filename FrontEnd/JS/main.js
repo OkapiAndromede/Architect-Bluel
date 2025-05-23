850e6ebe99bc4ed3505eb19903e70791a8e8c5f5
Neal Muller <neal.henri.muller@gmail.com>
2025-05-23 15:03:45 +0200

    refactorisation du code pour une meilleur maintenabilité

--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -1,18 +1,23 @@
-import { dataWorks, dataCategory } from "./api.js";
-import { worksGenerator, filtersGenerator, editorStyle } from "./dom.js";
-import { filterBtn } from "./util.js";
-console.log("Script chargé");
-//Importation des travaux
-const response = await fetch("http://localhost:5678/api/works");
-const works = await response.json();
-//Importation des catégorie
-const responseCategory = await fetch("http://localhost:5678/api/categories");
-const category = await responseCategory.json();
+import {
+  worksGenerator,
+  filtersGenerator,
+  removeEditorStyle,
+  displayEditorStyle,
+} from "./dom.js";
+
+//Importation des travaux depuis le serveur
+const responseWorkServer = await fetch("http://localhost:5678/api/works");
+const works = await responseWorkServer.json();
+//Importation des catégorie depuis le serveur
+const responseCategoryServer = await fetch(
+  "http://localhost:5678/api/categories"
+);
+const category = await responseCategoryServer.json();
 //Déclaration des variables globales
-let identification = localStorage.getItem("identifiant");
+let isAuthenticated = !!localStorage.getItem("identifiant");
 //Récupération des éléments du DOM
-const divEditor = document.querySelectorAll(".editor__mode");
 const divFilter = document.querySelector(".portfolio__filter");
+const hasBtnFilter = document.querySelectorAll("#portfolio button");
 //Supression des travaux encodé nativement dans le HTML
 document.querySelector(".gallery").innerHTML = ``;
 //Génération des travaux de façon dynamique
@@ -21,29 +26,32 @@ worksGenerator(works);
 const nomCategory = category.map((element) => element.name);
 //Unshift() est une méthode qui ajoute un élément au début d'un tableau
 nomCategory.unshift("Tous");
-//Affichage des filtres en fonction de mode editor
-if (!identification) {
+//Mise à jour de l'affichage en fonction du statut d'authentification
+
+if (!isAuthenticated) {
   filtersGenerator(nomCategory);
+  removeEditorStyle(isAuthenticated);
 } else {
   divFilter.innerHTML = "";
+  displayEditorStyle(isAuthenticated);
 }
-editorStyle(identification);
-//Programme filtre fonctionnel
-const button = document.querySelectorAll("#portfolio button");
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", (event) => {
-    button.forEach((btn) => btn.classList.remove("selected"));
+//Programme permettant de filter les travaux en fonction du filtre sélectionné
+if (hasBtnFilter) {
+  for (let i = 0; i < hasBtnFilter.length; i++) {
+    hasBtnFilter[i].addEventListener("click", (event) => {
+      hasBtnFilter.forEach((btn) => btn.classList.remove("selected"));
 
-    event.target.classList.add("selected");
-    const worksFiltree = works.filter((projet) => {
-      return projet.category.name === event.target.innerText;
+      event.target.classList.add("selected");
+      const worksFiltered = works.filter((projet) => {
+        return projet.category.name === event.target.innerText;
+      });
+      document.querySelector(".gallery").innerHTML = ``;
+
+      if (event.target.innerText !== "Tous") {
+        worksGenerator(worksFiltered);
+      } else {
+        worksGenerator(works);
+      }
     });
-    document.querySelector(".gallery").innerHTML = ``;
-
-    if (event.target.innerText !== "Tous") {
-      worksGenerator(worksFiltree);
-    } else {
-      worksGenerator(works);
-    }
-  });
+  }
 }
